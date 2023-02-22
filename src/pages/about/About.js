59701f542c660a5592b74e2c60ce38e9a4b1a7f6
FernandoRhenan <fernandorhenan9@gmail.com>
2023-02-22 14:48:23 -0300

    correcao de responsividade

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -6,26 +6,31 @@ const About = () => {
 
     const [info, setInfo] = useState(false)
     const showDetails = () => {
-     setInfo(Math.random())
+        setInfo(Math.random())
     }
 
 
     return (
         <div className={styles.about_container}>
             <h1>Sobre o portifólio</h1>
-            <p>Criei esse portifólio com o objetivo de divulgar e apresentar meu trabalho, nele está aplicado o muitas coisas que aprendi durante o tempo que estudo desenvolvimento web e também o que eu ainda desejo aprender.</p>
-            <p>Esse portifólio foi feito com muito amor. Dos arquivos principais até o plano de fundo foram feitos autenticamente por mim, usando minha capacidade criativa, imaginação e também a técnica.</p>
+
+            <div>
+                <p>Criei esse portifólio com o objetivo de divulgar e apresentar meu trabalho, nele está aplicado o muitas coisas que aprendi durante o tempo que estudo desenvolvimento web e também o que eu ainda desejo aprender.</p>
+                <p>Esse portifólio foi feito com muito amor. Dos arquivos principais até o plano de fundo foram feitos autenticamente por mim, usando minha capacidade criativa, imaginação e também a técnica.</p>
+            </div>
 
             <h2>Tecnologias usadas</h2>
             <h4>em termos mais técnicos</h4>
 
-            <p>
-                Para fazer o esse portifólio usei como base o React JS, que é uma biblioteca do JavaScript que utiliza de várias outras tecnologias para funcionar, como HTML, Node JS e o transpilador Babel, assim como várias outras dependências.
-                Foi usado também o React Router Dom para fazer o roteamento das paginas, mantendo o fundamento de <span className='info' onMouseOver={showDetails}>SPA</span>, e o React Icons, usado para obter todos os ícones do site.
-            </p>
-            <p>
-                O plano de fundo principal e o plano de fundo das cards, foram feitos no PhotoScape X, que é um editor de imagens que eu usava para editar algumas das minhas fotografias, então percebi que nele também seria possível criar fundos para os meus sites.
-            </p>
+            <div>
+                <p>
+                    Para fazer o esse portifólio usei como base o React JS, que é uma biblioteca do JavaScript que utiliza de várias outras tecnologias para funcionar, como HTML, Node JS e o transpilador Babel, assim como várias outras dependências.
+                    Foi usado também o React Router Dom para fazer o roteamento das paginas, mantendo o fundamento de <span className='info' onMouseOver={showDetails}>SPA</span>, e o React Icons, usado para obter todos os ícones do site.
+                </p>
+                <p>
+                    O plano de fundo principal e o plano de fundo das cards, foram feitos no PhotoScape X, que é um editor de imagens que eu usava para editar algumas das minhas fotografias, então percebi que nele também seria possível criar fundos para os meus sites.
+                </p>
+            </div>
 
             {info &&
                 <Alert cancelInfo={info} type='Info' title='SPA: ' content='Do inglês, single page application, ou seja, aplicação de página unica, isso significa que SPAs são paginas que não apresentam o tradicional carregamento, quando clicamos em um link que redireciona para uma outra área do site, por exemplo.' />
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -7,9 +7,9 @@ import Footer from '../../components/footer/Footer'
 const Profile = () => {
 
   const [info, setInfo] = useState(false)
- const showDetails = () => {
-  setInfo(Math.random())
- }
+  const showDetails = () => {
+    setInfo(Math.random())
+  }
 
 
   return (
@@ -18,27 +18,28 @@ const Profile = () => {
       <h1>Fernando Rhenan</h1>
       <h2>Web developer</h2>
 
-      <p>Me chamo Fernando Rhenan Weber, tenho 19 anos e estudo desenvolvimento Web há pelo menos 1 ano.
-        Entrei nessa área logo após a minha formação no ensino médio.
-      </p>
+      <div>
+        <p>Me chamo Fernando Rhenan Weber, tenho 19 anos e estudo desenvolvimento Web há pelo menos 1 ano.
+          Entrei nessa área logo após a minha formação no ensino médio.
+        </p>
 
-      <p>Comecei a estudar através de cursos em plataformas digitais e pela internet de forma geral. Então peguei gosto por programação e decidi definitivamente que eu faria disso minha profissão.</p>
+        <p>Comecei a estudar através de cursos em plataformas digitais e pela internet de forma geral. Então peguei gosto por programação e decidi definitivamente que eu faria disso minha profissão.</p>
 
-      <p>O foco dos meus estudos e trabalho estão na parte do <span className='info' onMouseOver={showDetails}>Front-end</span>
-        , pois é minha paixão trabalhar com coisas visuais e que transmitem uma sensação de coerência e aconchego aos olhos das pessoas.
-      </p>
+        <p>O foco dos meus estudos e trabalho estão na parte do <span className='info' onMouseOver={showDetails}>Front-end</span>
+          , pois é minha paixão trabalhar com coisas visuais e que transmitem uma sensação de coerência e aconchego aos olhos das pessoas.
+        </p>
 
-      <p>
-        Sempre gostei muito de fotografia, edição de fotos e música, então, foi por já ter uma boa afinidade com a arte, que juntei algumas habilidades com a vontade trabalhar com tecnologia e telas.
-      </p>
+        <p>
+          Sempre gostei muito de fotografia, edição de fotos e música, então, foi por já ter uma boa afinidade com a arte, que juntei algumas habilidades com a vontade trabalhar com tecnologia e telas.
+        </p>
+      </div>
 
-  
       <h2>Qual o meu objetivo?</h2>
+      <div>
+        <p>Meu principal objetivo é me aperfeiçoar como desenvolvedor front-end, aprender tudo o que é necessário para ter uma carreira de sucesso. Além disso, quero também aprender mais sobre back-end, gosto de entender como as coisas funcionam por trás dos "panos", e também acho importante ter a visão mais generalista sobre um sistema.</p>
 
-      <p>Meu principal objetivo é me aperfeiçoar como desenvolvedor front-end, aprender tudo o que é necessário para ter uma carreira de sucesso. Além disso, quero também aprender mais sobre back-end, gosto de entender como as coisas funcionam por trás dos "panos", e também acho importante ter a visão mais generalista sobre um sistema.</p>
-
-      <p>Quero aprender inglês de forma fluente. Fiz 1 ano de curso e saí quando a pandemia teve o grande aumento de casos, então depois disso não fiz mais nenhum curso de línguas, por conta de uma nova rotina de trabalho e de estudos tive que priorizar outras coisas.</p>
-
+        <p>Quero aprender inglês de forma fluente. Fiz 1 ano de curso e saí quando a pandemia teve o grande aumento de casos, então depois disso não fiz mais nenhum curso de línguas, por conta de uma nova rotina de trabalho e de estudos tive que priorizar outras coisas.</p>
+      </div>
       {info &&
         <Alert cancelInfo={info} type='Info' title='Front-end: ' content='Essa é uma das áreas do desenvolvimento web que trabalha com a parte visual de um site e proporciona uma boa experiencia para o consumidor final.' />
       }
